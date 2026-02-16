7036827d5b59e22adce59d45f023a2b34fb03c1d
shrikant1 <wshrikant2@gmail.com>
2026-02-16 12:18:27 +0530

    test change

--- src/exception.py
+++ src/exception.py
@@ -11,7 +11,7 @@ class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_detail=error_detail)
-
+ 
 
     def __str__(self):
         return self.error_message
